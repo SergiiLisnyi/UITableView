1a77a9b9437f0445c42f532b1543823771113e71
Sergii Lisnyi <Sergii_Lisnyi@epam.com>
2018-05-08 18:04:38 +0300

    correction some code in ArrayData

--- UITable/UITable/Model/ArrayData.swift
+++ UITable/UITable/Model/ArrayData.swift
@@ -13,25 +13,25 @@ class ArrayData : DataEntityProtocol {
     private let dataStack = DataStructureEntity(name: DataType.Stack.rawValue, type: .Stack,
                                                 url: getInfo(typeInfo: "url",typeData: .Stack), definition: getInfo(typeInfo: "descr",typeData: .Stack))
     
-    private let dataQueue = DataStructureEntity(name: DataType.Queue.rawValue, type: DataType.Queue,
+    private let dataQueue = DataStructureEntity(name: DataType.Queue.rawValue, type: .Queue,
                                                 url: getInfo(typeInfo: "url",typeData: .Queue), definition: getInfo(typeInfo: "descr",typeData: .Queue))
     
-    private let dataSet = DataStructureEntity(name: DataType.Set.rawValue, type: DataType.Set,
+    private let dataSet = DataStructureEntity(name: DataType.Set.rawValue, type: .Set,
                                               url: getInfo(typeInfo: "url",typeData: .Set), definition: getInfo(typeInfo: "descr",typeData: .Set))
     
-    private let dataDequeu = DataStructureEntity(name: DataType.Dequeu.rawValue, type: DataType.Dequeu,
+    private let dataDequeu = DataStructureEntity(name: DataType.Dequeu.rawValue, type: .Dequeu,
                                                  url: getInfo(typeInfo: "url",typeData: .Dequeu), definition: getInfo(typeInfo: "descr",typeData: .Dequeu))
     
-    private let dataPriorityQueue = DataStructureEntity(name: DataType.PriorityQueue.rawValue, type: DataType.PriorityQueue,
+    private let dataPriorityQueue = DataStructureEntity(name: DataType.PriorityQueue.rawValue, type: .PriorityQueue,
                                                         url: getInfo(typeInfo: "url",typeData: .PriorityQueue), definition: getInfo(typeInfo: "descr",typeData: .PriorityQueue))
     
-    private let dataList = DataStructureEntity(name: DataType.List.rawValue, type: DataType.List,
+    private let dataList = DataStructureEntity(name: DataType.List.rawValue, type: .List,
                                                url: getInfo(typeInfo: "url",typeData: .List), definition: getInfo(typeInfo: "descr",typeData: .List))
     
-    private let dataMultiSet = DataStructureEntity(name: DataType.MultiSet.rawValue, type: DataType.MultiSet,
+    private let dataMultiSet = DataStructureEntity(name: DataType.MultiSet.rawValue, type: .MultiSet,
                                                    url: getInfo(typeInfo: "url",typeData: .MultiSet), definition: getInfo(typeInfo: "descr",typeData: .MultiSet))
     
-    private let dataDictionary = DataStructureEntity(name: DataType.Dictionary.rawValue, type: DataType.Dictionary,
+    private let dataDictionary = DataStructureEntity(name: DataType.Dictionary.rawValue, type: .Dictionary,
                                                      url: getInfo(typeInfo: "url",typeData: .Dictionary), definition: getInfo(typeInfo: "descr",typeData: .Dictionary))
     
     
@@ -49,18 +49,11 @@ class ArrayData : DataEntityProtocol {
     }
     
     static func getInfo(typeInfo: String, typeData: DataType) -> String? {
-        var myDict: NSDictionary?
-        let pathPlist = Bundle.main.path(forResource: "InfoDataStructure", ofType: "plist")
-        if let path = pathPlist {
-            myDict = NSDictionary(contentsOfFile: path)
-        }
-        if let dict = myDict {
-            if let value = dict.value(forKey: typeData.rawValue) as? NSDictionary {
-                let url = value[typeInfo] as? String
-                return url
-            }
-        }
-        return nil
+        guard let path = Bundle.main.path(forResource: "InfoDataStructure", ofType: "plist") else { return nil }
+        guard let dict = NSDictionary(contentsOfFile: path) else { return nil }
+        guard let value = dict.value(forKey: typeData.rawValue) as? NSDictionary else { return nil }
+            let url = value[typeInfo] as? String
+            return url
     }
   
     var count: Int {
